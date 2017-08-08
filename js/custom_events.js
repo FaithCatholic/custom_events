f9aaafe66ec51a04c3787d1a3621336220142780
Nicholas Ketchum <nketchum@gmail.com>
2017-08-08 10:30:31 -0400

    add calendar_date field functionality

--- js/custom_events.js
+++ js/custom_events.js
@@ -4,7 +4,7 @@
    */
   Drupal.behaviors.exposedfilter_buttons = {
     attach: function(context, settings) {
-      // $('a.filter-item').on('click', function(e) {
+
       $('.calendar-calendar .month-view td a[rel]').on('click', function(e) {
         e.preventDefault();
         // Get the ID of clicked element.
@@ -12,30 +12,20 @@
         id_min = id + ' 00:00:00';
         id_max = id + ' 23:59:59';
         // Set the value in the select element.
-        var filter_min = $('#views-exposed-form-events-page-1 input[name="date[min]"]');
+        var filter_min = $('#views-exposed-form-events-calendar-page-1 input[name="date[min]"]');
         filter_min.val(id_min);
-        var filter_max = $('#views-exposed-form-events-page-1 input[name="date[max]"]');
+        var filter_max = $('#views-exposed-form-events-calendar-page-1 input[name="date[max]"]');
         filter_max.val(id_max);
         // Unset and re-set the active class.
         // $('.filter-item a').removeClass('active');
         // $(e.target).addClass('active');
         // Trigger the select.
-        $('#views-exposed-form-events-page-1 input[name="date[min]"]').trigger('change');
-        $('#views-exposed-form-events-page-1 input.form-submit').trigger('click');
+        $('#views-exposed-form-events-calendar-page-1 input[name="date[min]"]').trigger('change');
+        $('#views-exposed-form-events-calendar-page-1 input.form-submit').trigger('click');
+        // Scroll back to top of screen.
+        $('body, html').animate({scrollTop: 0}, 100);
       });
     }
   };
-
-  // jQuery(document).ajaxComplete(function(event, xhr, settings) {
-  //   switch(settings.extraData.view_name){
-  //     case "events":
-  //       var filter_id = $('#views-exposed-form-events-page-1 input[name="date[min]"]').val();
-  //       $('.filter-item a').removeClass('active');
-  //       $('.filter-item').find('#' + filter_id).addClass('active');
-  //       break;
-  //     default:
-  //       break;
-  //   };
-  // });
 
 })(jQuery);
